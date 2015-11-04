374f9f137b7985cc2b6bebf7e8f4a0a54ecb4fab
Wesley Kepke <wesleykepke@nevada.unr.edu>
2015-11-03 21:07:25 -0800

    added declaration node

---
+++ PA6/nodes/declaration_Node.cpp
@@ -0,0 +1,79 @@
+/*
+Name: Renee Iinuma, Kyle Lee, and Wesley Kepke. 
+File: declarator_Node.cpp
+Created: November 2, 2015
+Last Modified: November 2, 2015
+Class: CS 460 (Compiler Construction)
+
+This is the implementation file for the base AST node class of our C compiler.  
+*/
+
+#include "declaration_Node.h"
+
+/*
+Function: declaration_Node(astNode* A, astNode* B) (constructor) 
+
+Description: 
+*/
+declaration_Node::declaration_Node(astNode* A, astNode* B) : astNode(){
+	exprA = A;
+	exprB = B;
+}
+
+/*
+Function: gen3AC()
+
+Description: 
+*/
+void declaration_Node::gen3AC(){
+	std::cout << "Generate 3AC for declarator node" << std::endl;
+}
+
+/*
+Function: print(int indent)
+
+Description: 
+*/
+void declaration_Node::print(int indent){
+
+	for(int i = 0; i < indent; i++){
+		std::cout << '\t';
+	}
+	std::cout << "Prefix Expression Node:" << std::endl;
+	
+	for(int i = 0; i < indent; i++){
+		std::cout << '\t';
+	}
+	std::cout << "A: ";
+	if( exprA != NULL ){
+		exprA->print(indent + 1);
+		//std::cout << "AST Node";
+	}
+	else{
+		std::cout << "NULL ";
+	}
+
+	std::cout << std::endl;
+	for(int i = 0; i < indent; i++){
+		std::cout << '\t';
+	}
+	std::cout << "B: ";
+	if( exprB != NULL ){
+		exprB->print(indent+1) ;
+		//std::cout << "AST Node";
+	}
+	else{
+		std::cout << "NULL ";
+	}
+}
+
+/*
+Function: ~declarator_Node() (destructor) 
+
+Description: 
+*/
+declaration_Node::~declaration_Node(){
+	exprA = NULL;
+	exprB = NULL; 
+}
+
