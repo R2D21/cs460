26c9a7fdfec189bb23a24d2109f23c43fd97896c
rtiinuma <rtiinuma@nevada.unr.edu>
2015-11-03 21:28:44 -0800

    Command line

--- PA6/main/main.cpp
+++ PA6/main/main.cpp
@@ -15,6 +15,30 @@ This is the main file for the front end of our ANSI C compiler.
 #include <stdlib.h>
 using namespace std;
 
+#include "optionparser.h"
+
+// Command line parser
+enum  optionIndex { UNKNOWN, 
+					HELP, 
+					ASSEM, 
+					AC,
+					COMP,
+					OUT,
+					DEBUG };
+const option::Descriptor usage[] =
+{
+	{HELP, 0,"", "help",option::Arg::None, "\t--help  \tPrint usage and exit." },
+	{ASSEM, 0,"S","assembly",option::Arg::None, "\t--assembly, -S  \tOutput assembly." },
+	{AC, 0, "q", "3AC", option::Arg::None,"\t--3AC, -q  \tOutput 3 address code."},
+	{COMP, 0, "c", "compile", option::Arg::Required,"\t--compile <file>, -c <file> \tCompile <file>."},
+	{OUT, 0, "o", "out", option::Arg::Required,"\t--output <file>, -o <file> \tOutput to <file>."},
+	{DEBUG, 0, "d", "", option::Arg::Required,"\t-d[l|y|s] \tOutput lex/yacc/symbol table."},
+	{0,0,0,0,0,0}
+};
+
+
+
+
 // externs 
 extern int yyparse();
 extern FILE* yyin; 
@@ -30,17 +54,90 @@ extern int yylineno;
 extern bool endline;
 vector<string> sourceCode;
 
-int main(int argc, char** argv) {
-	// #todo - add compiler flags 
+int main(int argc, char* argv[]) {
 
-	bool fileSpecificed = false;
+	bool fileSpecified = false;
+	char inputFile[256];
 	string filename;
+	ofstream outFile;
+	int j;
+	char input;
 
+	// Command line
+	argc-=(argc>0); argv+=(argc>0);
+	option::Stats  stats(usage, argc, argv);
+	std::vector<option::Option> options(stats.options_max);
+	std::vector<option::Option> buffer(stats.buffer_max);
+  	option::Parser parse(usage, argc, argv, &options[0], &buffer[0]);
 
+	if (parse.error())
+		return 1;
 
+	if (options[HELP] || argc == 0) {
+	    option::printUsage(std::cout, usage);
+	    return 0;
+	}
+
+	for (int i = 0; i < parse.optionsCount(); ++i)
+	{
+	    option::Option& opt = buffer[i];
+	    switch (opt.index())
+	    {
+			case AC:
+				cout << "3 address code TBD." << endl;
+				break;
+
+			case ASSEM:
+				cout << "Assembly code TBD." << endl;
+				break;
+			case COMP:
+				//inputFile = opt.arg;
+				strcpy(inputFile, opt.arg);
+				fileSpecified = true;
+				break;
+			case OUT:
+				filename = opt.arg;
+				filename += "../outputFiles/";
+				outFile.open(filename.c_str());
+				outFile << "Final output will be here." << endl;
+				outFile.close();
+				break;
+			case DEBUG:
+				j = 0;
+				input = opt.arg[j];
+				while(input != '\0'){
+					switch(input){
+						case 'l':
+							LFLAG = true;
+							outL.open("../outputFiles/tokens.txt", std::ofstream::out);
+							break;
+						case 'y':
+							YFLAG = true;
+							outY.open("../outputFiles/reductions.txt", std::ofstream::out);
+							break;
+						case 's':
+							//TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+							break;
+						default:
+							break;
+					}
+					j++;
+					input = opt.arg[j];
+				}
+				break;
+	      	default:
+	       		break;
+	    }
+	}
 	// read input file
 
-	ifstream fin (argv[1]);
+	if( !fileSpecified ){
+		cout << "Error: Please enter file name (See --help)." << endl;
+		return 0;
+	}
+
+	// Read in source code
+	ifstream fin (inputFile);
 	char str[256];
 	while( fin.good() ){	
 		fin.getline(str, 256);
@@ -49,54 +146,15 @@ int main(int argc, char** argv) {
 	}
 	fin.close();
 
-	yyin = fopen(argv[1], "r");
-	for(int i=1; i < argc; i++){
-		
-		if( (string) argv[i] == "-o" ){
-			if(i+1 < argc){
-				filename = argv[i+1];
-			}
-			else{
-				cout << "Please specify output file:";
-				cin >> filename;
-			}
-			fileSpecificed = true;
-		}
-		if(((string)argv[i]).find("-d") != string::npos)
-		{
-			if( ((string)argv[i]).find("l") != string::npos){
-				LFLAG = true;
-			}
-			if( ((string)argv[i]).find("y") != string::npos){
-				YFLAG = true;
-			}
-		}
-	}
+	// Set lexer input
+	yyin = fopen(inputFile, "r");
+
 
 	//outG.open("graph.dot");
 	outG << "digraph{\n";
 
 
-	// check if 
-	if (LFLAG){
-		if(fileSpecificed){
-			filename = "../outputFiles/" + filename + "_token.txt";
-		}
-		else{
-			filename = "../outputFiles/tokens.txt";
-		}
-		outL.open(filename.c_str(), std::ofstream::out);
-	}
-	if (YFLAG){
-		if(fileSpecificed){
-			filename = "../outputFiles/" + filename + "_reductions.txt";
-		}
-		else{
-			filename = "../outputFiles/reductions.txt";
-		}
-		outY.open(filename.c_str(), std::ofstream::out);
-	}
-
+	cout << "Main program - compiler driver." << endl;
 	if(LFLAG){
 		outL << std::endl << "=================================================================" << std::endl;
 		outL << "Line #" << yylineno << ": " << std::endl;
@@ -108,15 +166,17 @@ int main(int argc, char** argv) {
 		outY << sourceCode[0] << std::endl;
 	}
 
-	cout << "Main program - compiler driver." << endl;
-
+	// Parser
 	yyparse();  
 
 	cout << "Parse complete" << endl;
+
+	// Finish graph
 	outG << "}" << endl;
 	outG.close();
 	system("dot -Tpng graph.dot -o graph.png");
 	system("gnome-open graph.png");
+
 	if (LFLAG) {
 		outL.close();
 	}
